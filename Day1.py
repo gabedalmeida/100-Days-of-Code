84a93d4b8ce319f4fd7c7d70499971c5338e7c10
gabedalmeida <gabrelalmeida17@icloud.com>
2024-01-08 16:28:45 +0100

    Day1

---
+++ Day1.py
@@ -0,0 +1,5 @@
+#print("Hello " + input("What is your name?"))
+city_name = input('which city did you grow up in?\n')
+pet_name = input("What is your last pet's name?\n")
+
+print('Ok, your band name could be: '+ city_name +" "+ pet_name)
