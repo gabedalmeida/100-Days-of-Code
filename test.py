d8aa56c1652e2a7971884d7da40095b3a31b0422
gabedalmeida <gabrelalmeida17@icloud.com>
2024-09-15 15:08:08 +0200

    adjustment

--- test.py
+++
@@ -1,8 +0,0 @@
-with open("/Users/gabrielalmeida/Desktop/my_file.txt") as file:
-    #or ../../../../../my_file.txt
-    #/Users/gabrielalmeida/Desktop/Projetos/100-Days-of-Code/my_file.txt
-    contents = file.read()
-    #mode = r read, =a apend, w write
-    print(contents)
-    
-#/Users/gabrielalmeida/Desktop/Projetos/100-Days-of-Code/INTERMEDIATE/DAY24
